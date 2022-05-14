9a1f8b5ddef188402b5bcf80112d22bab4c63a16
dylan-devs <GCAS022117@coderacademy.edu.au>
2022-05-14 19:30:56 +1000

    function added in app.js; bug: review tomorrow morning

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ const sectBtn = document.querySelectorAll('.control'); // actual button
 
 // select body as a class of main content 
 const allSections = document.querySelectorAll('.main-content')
+
+
+// function: min 27 onwards: review
+function PageTransitions() {
+    //button click active class: want to remove class that is not active
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i] = .addEventListener('click', () => {
+            let currentBtn = document.querySelectorAll('.active-btn');
+                currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+                this.className += ' active-btn';
+        } )
+    }
+}
+
+PageTransitions();
